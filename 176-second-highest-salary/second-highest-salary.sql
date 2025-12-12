af2b18fb443a54c2091c66889f971fc0a419c6a5
Biliarto Sastro Cemerson <139372393+billycemerson@users.noreply.github.com>
2025-12-12 14:17:24 +0700

    Time: 598 ms (5.01%) | Memory: 0B (100.00%) - LeetSync

---
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -0,0 +1,14 @@
+# Write your MySQL query statement below
+WITH Ranking AS (
+    SELECT salary,
+        RANK() OVER (
+            ORDER BY salary DESC
+        ) AS new_rank
+    FROM Employee
+)
+SELECT COALESCE(
+    (SELECT salary
+    FROM Ranking
+    WHERE new_rank>1
+    LIMIT 1)
+) AS SecondHighestSalary;
